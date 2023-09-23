4b1670728525e365fd435301103d1be7f4d514ac
adnanmulalic <adnan.mulalic3@gmail.com>
2023-09-23 11:37:36 +0200

    calculations and input working, styling not done

--- src/shuntingyardalgo.js
+++ src/shuntingyardalgo.js
@@ -1,12 +1,33 @@
+function operations(a, b, operator) {
+    let result = 0;
+    switch (operator) {
+        case "+":
+            result = Number(a) + Number(b);
+            return String(result);
+        case "-":
+            result = Number(a) - Number(b);
+            return String(result);
+        case "*":
+            result = Number(a) * Number(b);
+            return String(result);
+        case "/":
+            result = parseFloat(a) / parseFloat(b);
+            return String(result);
+    }
+}
+
 function shuntingyardalgo(expression) {
     const operators = [];
     const operands = [];
     let num = "";
     for (let i = 0; i < expression.length; i++) {
-        if (expression[i].match(/\d/)) {
+        if (expression[i].match(/[\d.]/)) {
             num += expression[i];
+        if (((i === 1 && operators[0] === "-"))) {
+            operators.shift();
+            num = "-" + num;
+        }
         if (operators.length >= 2 && operators[0] === "-" && expression[i - 2].match(/[-+*/]/)) {
-            console.log("negative num", num)
             operators.shift();
             num = "-" + num;
         }
@@ -18,8 +39,16 @@ function shuntingyardalgo(expression) {
     }
     operands.push(num);
 
+    operators.forEach(operator => {
+            let i = operands.length - 1;
+            let a = operands[i]; let b = operands[i - 1];
+            operands.pop(); operands.pop();
+            operands.push(operations(a, b, operator));
+            console.log(operations(a, b, operator))
+    })
+
     console.log(num)
-    console.log(operands, operators);
+    console.log(operands);
 }
 
 export {shuntingyardalgo}
